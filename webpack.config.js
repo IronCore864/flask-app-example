04fcd74aa12e57f32e72c74481cceda964efa140
Tiexin Guo <tiexin.guo@kuehne-nagel.com>
2017-01-27 16:44:35 +0200

    minor twix about style

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+      },
+      {
+        test: /\.less$/,
+        loader: ExtractTextPlugin.extract("style-loader", "css-loader!less-loader")
+      }
+    ]
+  }
+}
